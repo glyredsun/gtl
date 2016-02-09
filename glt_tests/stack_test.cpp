fc95e12f5a865d6ed8957048f0a1cf5ebb1188a5
gly <glyredsun@163.com>
2016-02-09 19:13:46 +0800

    some modifying

--- glt_tests/stack_test.cpp
+++ glt_tests/stack_test.cpp
@@ -1,1 +1,10 @@
 #include <stack.hpp>
+
+#include <stack>
+
+int main(void)
+{
+	std::stack<int> s;
+
+	return 0;
+}
--- gtl/macros.hpp
+++ gtl/macros.hpp
@@ -1,4 +1,11 @@
+#ifdef _MSC_VER
+#pragma once
+#endif // _MSC_VER
+
+#ifndef _MACROS_HPP_
 
 #define	NS_BEGIN(ns)	namespace ns {
 
 #define NS_END(ns)		};
+
+#endif // !_MACROS_HPP_
