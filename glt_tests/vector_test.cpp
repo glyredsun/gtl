fe642530733be93989bed201b27602afcdc47def
gly <glyredsun@163.com>
2016-03-11 14:04:16 +0800

    change vector.hpp

--- glt_tests/vector_test.cpp
+++ glt_tests/vector_test.cpp
@@ -1,8 +1,21 @@
 #include <vector.hpp>
 
+#include <iostream>
+
 int main(void)
 {
 	
+	gtl::vector<int> v;
+	for (size_t i = 0; i < 100; i++)
+	{
+		v.push_back(i);
+	}
+
+	size_t s = 0;
+	for (gtl::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		std::cout << ++s << ":" << *it << std::endl;
+	}
 
 	return 0;
 }
--- gtl/vector.hpp
+++ gtl/vector.hpp
@@ -1,5 +1,5 @@
 #include <macros.hpp>
-#include <algorithm.hpp>
+#include <algorithm>
 
 NS_BEGIN(gtl)
 
@@ -7,7 +7,7 @@ template<typename T>
 class vector
 {
 public:
-	vector(size_t initSize)
+	vector(size_t initSize = 0)
 		: _size{ initSize }
 		, _capacity{ initSize + SPARE_SIZE }
 	{
@@ -44,7 +44,7 @@ public:
 	vector& operator=(const vector& other)
 	{
 		if (this != &other) {
-			vector copy = other;
+			vector copy(other);
 			std::swap(*this, copy);
 		}
 		return *this;
@@ -61,21 +61,124 @@ public:
 	}
 
 	T& operator[](size_t idx) {
-		assert(idx < _size, "out of range");
 		return _datas[idx];
 	}
 
 	const T& operator[](size_t idx) const {
-		assert(idx < _size, "out of range");
 		return _datas[idx];
 	}
 
 	void resize(size_t newSize) {
-
+		if (newSize > _capacity) {
+			reserve(newSize * 2);
+		}
+		_size = newSize;
 	}
 
-	void reserve(size_t capacity) {
-		if (_capacity >)
+	void reserve(size_t newCapacity) {
+		if (newCapacity <= _size) {
+			return;
+		}
+
+		T *newDatas = new T[newCapacity];
+
+		for (size_t i = 0; i < _size; i++)
+		{
+			newDatas[i] = std::move(_datas[i]);
+		}
+
+		_capacity = newCapacity;
+		delete[] _datas;
+		_datas = newDatas;
+	}
+
+	void push_back(T&& data) {
+		resize(_size + 1);
+		_datas[_size - 1] = data;
+	}
+
+	void push_back(const T& data) {
+		T copy = data;
+		push_back(std::move(copy));
+	}
+
+public:
+	class iterator
+	{
+	public:
+		iterator(vector &v, size_t idx)
+			: vectPtr(&v), idx(idx)
+		{
+
+		}
+
+		iterator operator ++(int)
+		{
+			iterator copy = *this;
+			++idx;
+			return copy;
+		}
+
+		iterator& operator ++()
+		{
+			++idx;
+			return *this;
+		}
+
+		iterator& operator =(const iterator& other)
+		{
+			if (&other !== this)
+			{
+				this->vectPtr = other.vectPtr;
+				this->idx = other.idx;
+			}
+			return *this;
+		}
+
+		T& operator ->()
+		{
+			return (*vectPtr)[idx];
+		}
+
+		const T& operator ->() const
+		{
+			return (*vectPtr)[idx];
+		}
+
+		T& operator*()
+		{
+			return (*vectPtr)[idx];
+		}
+
+		bool operator == (const iterator& other)
+		{
+			if (&other == this)
+			{
+				return true;
+			}
+			return vectPtr == other.vectPtr && idx == other.idx;
+		}
+
+		bool operator != (const iterator &other)
+		{
+			return !(*this == other);
+		}
+
+	private:
+		vector *vectPtr;
+		size_t idx;
+	};
+
+public:
+
+	iterator begin()
+	{
+		return iterator(*this, 0);
+	}
+
+	iterator end()
+	{
+		return iterator(*this, _size);
 	}
 
 private:
