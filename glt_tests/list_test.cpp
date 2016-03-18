3c89f827813e07b740528568206ee7ac1bff4809
gly <glyredsun@163.com>
2016-03-18 20:35:38 +0800

    add list_test

--- glt_tests/list_test.cpp
+++ glt_tests/list_test.cpp
@@ -1,1 +1,26 @@
 #include <list.hpp>
+
+#include <iostream>
+
+#include <list>
+int main(void)
+{
+	gtl::list<int> *il = new gtl::list<int>;
+	il->push_back(1);
+	il->push_front(2);
+	il->push_back(3);
+	il->push_front(4);
+
+	std::list<int> l;
+	il->pop_back();
+	//while (!il.empty())
+	//{
+	//	std::cout << il.front() << std::endl;
+	//	il.pop_front();
+	//}
+ 	system("pause"); 
+	delete il;
+	system("pause");
+	return 0;
+}
+ 
--- gtl/list.hpp
+++ gtl/list.hpp
@@ -5,17 +5,102 @@
 #ifndef _LIST_HPP_
 #define _LIST_HPP_
 
+#include <macros.hpp>
+
+NS_BEGIN(gtl);
+
 template <typename ElemType>
-class List
+class list
 {
 public:
+
+	list() : _header(ElemType(), &_header, &_header)
+	{
+	}
+
+	~list()
+	{
+		Node *p = _header.next;
+		while (p != &_header)
+		{
+			Node *next = p->next;
+			delete p;
+			p = next;
+		}
+	}
+
+	void push_back(const ElemType &elem)
+	{
+		Node *newNode = new Node(elem, _header.pre, &_header);
+		_header.pre->next = newNode;
+		_header.pre = newNode;
+	}
+
+	void push_front(const ElemType &elem)
+	{
+		Node *newNode = new Node(elem, &_header, _header.next);
+		_header.next->pre = newNode;
+		_header.next = newNode;
+	}
+
+	ElemType &front()
+	{
+		return _header.next->elem;
+	}
+
+	const ElemType &front() const
+	{
+		return _header.next->elem;
+	}
+
+	ElemType &back()
+	{
+		return _header.pre->elem;
+	}
+
+	const ElemType &back() const
+	{
+		return _header.pre->elem;
+	}
+
+	void pop_front()
+	{
+		Node *frontNode = _header.next;
+		_header.next = frontNode->next;
+		frontNode->next->pre = &_header;
+		delete frontNode;
+	}
+
+	void pop_back()
+	{
+		Node *backNode = _header.pre;
+		_header.pre = backNode->pre;
+		backNode->pre->next = &_header;
+		delete backNode;
+	}
+
+	bool empty()
+	{
+		return _header.next == &_header;
+	}
 
 private:
 	struct Node {
 		ElemType elem;
 		Node *pre;
 		Node *next;
+
+		Node(const ElemType &elem, Node *pre, Node *next) : elem(elem), pre(pre), next(next) {
+			printf("%s %p\n", __FUNCTION__, this);
+		}
+		~Node() {
+			printf("%s %p\n", __FUNCTION__, this);
+		}
 	};
+	Node _header;
 };
+
+
+NS_END(gtl);
 
 #endif // ! _LIST_HPP_
