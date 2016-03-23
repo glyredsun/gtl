fe90fbe9e55f4336e54c12dfdb0df422076ce7b4
gly <glyredsun@163.com>
2016-03-23 18:06:45 +0800

    c list

--- glt_tests/list_test.cpp
+++ glt_tests/list_test.cpp
@@ -29,6 +29,45 @@ int main(void)
  	system("pause"); 
 	delete il;
 	system("pause");
+
+	printf("c list test\n");
+	struct list_head h = LIST_HEAD_INIT(h);
+	struct S
+	{
+		int n;
+		struct list_head list;
+	};
+	S s1 = {1, LIST_HEAD_INIT(s1.list)};
+	S s2 = {2, LIST_HEAD_INIT(s2.list)};
+	S s3 = {3, LIST_HEAD_INIT(s3.list)};
+	S s4 = {4, LIST_HEAD_INIT(s4.list)};
+	S s5 = {5, LIST_HEAD_INIT(s5.list) };
+	list_add(&s1.list, &h);
+	list_add(&s2.list, &h);
+	list_add(&s3.list, &h);
+	list_add(&s4.list, &h);
+	list_add_tail(&s5.list, &h);
+	struct list_head *p;
+	struct list_head *n;
+	S *tmp;
+	list_foreach_safe(p, n, &h)
+	{
+		tmp = container_of(p, S, list);
+		printf("%d\n", tmp->n);
+		if (tmp->n == 2 || tmp->n == 5) {
+			list_del(p);
+		}
+	}
+
+	putchar('\n');
+
+	list_foreach_prev(p, &h)
+	{
+		tmp = container_of(p, S, list);
+		printf("%d\n", tmp->n);
+	}
+
+	system("pause");
 	return 0;
 }
  
--- gtl/list.h
+++ gtl/list.h
@@ -9,12 +9,60 @@
 extern "C" {
 #endif
 
-struct list {
-	struct list *prev;
-	struct list *next;
+#ifndef offset_of
+#define offset_of(type, member)	(int)(&(((type*)0)->member))
+#endif // !offset_of
+
+#ifndef container_of
+#define container_of(ptr, type, member)		(type*)((const char*)ptr - offset_of(type, member))
+#endif // !container_of
+
+#define	list_entry(ptr, type, member)	container_of(ptr, type, member)
+
+struct list_head {
+	struct list_head *prev;
+	struct list_head *next;
 };
 
-#define	LIST_HEADER_INIT(h)	{&(h), &(h)}	
+#define	LIST_HEAD_INIT(name)	{&(name), &(name)}
+
+#define LIST_HEAD(name) \
+	struct list_head name = LIST_HEAD_INIT(name);
+
+static inline void list_add(struct list_head *n, struct list_head *head)
+{
+	head->next->prev = n;
+	n ->next = head->next;
+	head->next = n;
+	n->prev = head;
+}
+
+static inline void list_add_tail(struct list_head *n, struct list_head *head)
+{
+	head->prev->next = n;
+	n->prev = head->prev;
+	head->prev = n;
+	n->next = head;
+}
+
+static inline void list_del(struct list_head *h)
+{
+	h->prev->next = h->next;
+	h->next->prev = h->prev;
+	h->next = h->prev = h;
+}
+
+#define list_foreach(pos, head) \
+	for (pos = (head)->next; pos != (head); pos = pos->next)
+
+#define list_foreach_safe(pos, n, head) \
+	for (pos = (head)->next, n = pos->next; pos != (head); pos = n, n = pos->next)
+
+#define list_foreach_prev(pos, head) \
+	for (pos = (head)->prev; pos != (head); pos = pos->prev)
+
+#define list_foreach_prev_safe(pos, n, head) \
+	for (pos = (head)->prev, n = pos->prev; pos != (head); pos = n, n = pos->prev)
 
 #ifdef __cplusplus
 };
