a32c4d3ff88d375f85573177c85dd8d90e78eda3
gly <glyredsun@163.com>
2016-03-31 17:18:34 +0800

    add avltree code

--- glt_tests/bstree_test.cpp
+++ glt_tests/bstree_test.cpp
@@ -4,6 +4,7 @@
 
 int main(void)
 {
+	std::cout << "int" << std::endl;
 	gtl::bstree<int> t;
 	t.insert(19);
 	t.insert(11);
@@ -23,6 +24,14 @@ int main(void)
 	t1.midorderTraverse([](int n) {
 		std::cout << n << std::endl;
 	});
+
+	std::cout << "string" << std::endl;
+	gtl::bstree<std::string> ts;
+	ts.insert("dasfase");
+	ts.insert("23");
+	ts.insert("3rfgrgasdf");
+	ts.insert("dasfrg123");
+	ts.insert("y72124");
 
 	system("pause");
 	return 0;
