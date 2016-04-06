2f5e86488c2f458e12722310541534bd9892b275
gly <glyredsun@163.com>
2016-04-06 11:37:51 +0800

    complete avltree

--- glt_tests/avltree_test.cpp
+++ glt_tests/avltree_test.cpp
@@ -1,1 +1,17 @@
 #include <avltree.hpp>
+
+int main(void)
+{
+	gtl::avltree<int> t;
+
+	for (int i = 0; i < 31; i++)
+	{
+		t.insert(i);
+		t.print();
+		std::cout << std::endl;
+	}
+
+	system("pause");
+
+	return 0;
+}
--- gtl/avltree.hpp
+++ gtl/avltree.hpp
@@ -5,18 +5,62 @@
 #ifndef _AVLTREE_HPP_
 #define _AVLTREE_HPP_
 
+#include <macros.hpp>
+#include <algorithm.hpp>
+
+#include <iostream>
+
+NS_BEGIN(gtl);
+
 template <typename ElemType>
 class avltree
 {
+public:
+
+	void insert(const ElemType &elem)
+	{
+		insert(std::move(ElemType(elem)), _root);
+	}
+
+	void insert(ElemType &&elem)
+	{
+		insert(std::move(elem), _root);
+	}
+
+	void print()
+	{
+		print(_root, 0);
+	}
+
 protected:
+
 	struct Node
 	{
 		ElemType elem;
 		Node *left, *right;
-		int heigh;
+		int height;
 		Node(const ElemType &elem, Node *left = nullptr, Node *right = nullptr, int height = 0) : elem{ elem }, left{ left }, right{ right }, height{ height } { }
-		Node(ElemType &&elem, Node *left = nullptr, Node *right = nullptr, int height = 0) : elem{ elem }, left{ left }, right{ right }, height{ height } { }
+		Node(ElemType &&elem, Node *left = nullptr, Node *right = nullptr, int height = 0) : elem{ std::move(elem) }, left{ left }, right{ right }, height{ height } { }
 	};
+
+	static void print(Node *root, int depth)
+	{
+		if (root == nullptr)
+		{
+			std::cout << std::endl;
+			return;
+		}
+
+		print(root->left, depth + 1);
+
+		for (int i = 0; i < depth; i++)
+		{
+			std::cout << "\t";
+		}
+		std::cout << root->elem;
+
+		print(root->right, depth + 1);
+	}
 
 	static int height(Node *root)
 	{
@@ -57,7 +101,7 @@ protected:
 				doubleWithRightChild(t);
 		}
 		
-		root->height = 1 + max(height(root->left), height(root->right));
+		t->height = 1 + max(height(t->left), height(t->right));
 	}
 
 	static void rotateWithLeftChild(Node* &k2)
@@ -73,14 +117,14 @@ protected:
 	static void rotateWithRightChild(Node* &k2)
 	{
 		Node *k1 = k2->right;
-		k2->right = = k1->left;
+		k2->right = k1->left;
 		k1->left = k2;
 		k2->height = max(height(k2->left), height(k2->right)) + 1;
 		k1->height = max(height(k1->left), k2->height) + 1;
 		k2 = k1;
 	}
 
-	static doubleWithLeftChild(Node* &k3)
+	static void doubleWithLeftChild(Node* &k3)
 	{
 		Node *k1 = k3->left;
 		Node *k2 = k1->right;
@@ -97,12 +141,29 @@ protected:
 		k3 = k2;
 	}
 
-	static doubleWithRightChild(Node* &k3)
+	static void doubleWithRightChild(Node* &k3)
 	{
+		Node *k1 = k3->right;
+		Node *k2 = k1->left;
 
+		k3->right = k2->left;
+		k1->left = k2->right;
+		k2->left = k3;
+		k2->right = k1;
+
+		k1->height = max(height(k1->left), height(k1->right)) + 1;
+		k3->height = max(height(k3->left), height(k3->right)) + 1;
+		k2->height = max(k1->height, k3->height) + 1;
+
+		k3 = k2;
 	}
 
 	static const int ALLOWED_IMBALANCE = 1;
+
+private:
+	Node *_root{nullptr};
 };
+
+NS_END(gtl);
 
 #endif
