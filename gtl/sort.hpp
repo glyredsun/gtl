653930305bcb38a288f407510456de216f591cd4
gly <glyredsun@163.com>
2016-06-30 21:17:23 +0800

    remove test code

--- gtl/sort.hpp
+++ gtl/sort.hpp
@@ -4,15 +4,15 @@
 template <typename Iterator, typename Comparator>
 void insertionSort(const Iterator &begin, const Iterator &end, Comparator lessThan)
 {
-	if (begin == end)
+	if (begin == end) {
 		return;
+	}
 
 	Iterator j;
 
 	for (Iterator p = begin + 1; p != end; ++p)
 	{
 		auto tmp = std::move(*p);
-		int times = 0;
 		for (j = p; j != begin && lessThan(tmp, *(j - 1)); --j)
 		{
 			*j = std::move(*(j - 1));
