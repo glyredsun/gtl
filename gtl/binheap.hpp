03bd8836317b93de27b662d796613243ff37caba
glyredsun <glyredsun@163.com>
2016-07-03 18:27:12 +0800

    add binheap

---
+++ gtl/binheap.hpp
@@ -0,0 +1,19 @@
+#ifdef _MSC_VER
+#pragma once
+#endif // _MSC_VER
+#ifndef _BINHEAP_HPP_
+#define _BINHEAP_HPP_
+
+#include <macros.hpp>
+
+NS_BEGIN(gtl)
+
+template <typename ElemType>
+class binheap
+{
+
+};
+
+NS_END(gtl)
+
+#endif // !_BINHEAP_HPP_
