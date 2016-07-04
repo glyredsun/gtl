b4cd6c779348e670b91e4cc36380180327fabf00
glyredsun <glyredsun@163.com>
2016-07-05 07:39:49 +0800

    add push method to binheap

--- gtl/binheap.hpp
+++ gtl/binheap.hpp
@@ -5,13 +5,63 @@
 #define _BINHEAP_HPP_
 
 #include <macros.hpp>
+#include <vector.hpp>
+
+#include <functional>
 
 NS_BEGIN(gtl)
 
-template <typename ElemType>
+template <typename ElemType, typename Comparator = std::function<bool (const ElemType &, const ElemType &)>>
 class binheap
 {
+public:
+	binheap(int capacity = 64) : _size{ 0 }, _elems{ capacity } {}
 
+	size_t size() { return _size; }
+
+	bool empty() { return size() == 0; }
+
+	void push(const ElemType & elem)
+	{
+		push(elem, [](const ElemType &a, const ElemType &b) { return a < b; });
+	}
+
+	void push(const ElemType &elem, const Comparator &lessThan)
+	{
+		ElemType copy = elem;
+		push(std::move(elem), lessThan);
+	}
+
+	void push(const ElemType &&elem)
+	{
+		push(std::move(elem), [](const ElemType &a, const ElemType &b) { return a < b; });
+	}
+
+	void push(const ElemType &&elem, const Comparator &lessThan)
+	{
+		if (_size == _elems.size() - 1) 
+		{
+			_elems.resize(_elems.size() * 2);
+		}
+
+		int hole = ++_size;
+		
+		for (; hole > 1 && lessThan(elem, _elems[hole / 2]); hole /= 2)
+		{
+			_elems[hole] = std::move(_elems[hole / 2]);
+		}
+
+		_elems[hole] = std::move(elem);
+	}
+
+	const vector<ElemType> & container() const
+	{
+		return _elems;
+	}
+
+private:
+	size_t _size;
+	vector<ElemType> _elems;
 };
 
 NS_END(gtl)
--- gtl_tests/heap_test.cpp
+++ gtl_tests/heap_test.cpp
@@ -1,1 +1,29 @@
 #include "binheap.hpp"
+
+#include <iostream>
+
+#include <cstdlib>
+
+int main(void)
+{
+	gtl::binheap<int> h;
+
+	h.push(9);
+	h.push(1);
+	h.push(5);
+	h.push(6);
+	h.push(11);
+	h.push(956);
+	h.push(61);
+	h.push(95);
+	h.push(24);
+	h.push(436);
+
+	for (size_t i = 0; i < h.size(); ++i)
+	{
+		std::cout << h.container()[i + 1] << std::endl;
+	}
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
