3fe4144531f9c7f1663542a00963745a1b524e6e
glyredsun <glyredsun@163.com>
2016-07-14 07:32:12 +0800

    improve mergeSort

--- gtl/sort.hpp
+++ gtl/sort.hpp
@@ -55,30 +55,57 @@ void shellSort(const Iterator &begin, const Iterator &end)
 	shellSort(begin, end, [](const decltype(*begin) first, const decltype(*begin) second) { return first < second; });
 }
 
-template <typename Iterator>
-void merge(Iterator first, Iterator last, gtl::vector<decltype(*Iterator)>::iterator bufFirst, gtl::vector<decltype(*Iterator)>::iterator bufLast)
+template <typename Iterator, typename BufType, typename Comparator>
+void merge(Iterator first, Iterator last, BufType::iterator bufFirst, Comparator lessThan)
 {
-	
+	int count = gtl::distance(first, last);
+	Iterator left = first;
+	Iterator leftEnd = first + cout / 2;
+	Iterator right = leftEnd + 1;
+	Iterator rightEnd = last;
+	Iterator bufCur = bufFirst;
+
+	while (left <= leftEnd && right <= rightEnd) {
+		if (lessThan(*left, *right))
+			*bufCur++ = *left++;
+		else
+			*bufCur++ = *right++;
+	}
+
+	while (left <= leftEnd)
+		*bufCur++ = *left++;
+
+	while (right <= rightEnd)
+		*bufCur++ = *right++;
+
+
 }
 
-template <typename Iterator>
-void mergetSort(Iterator first, Iterator last, gtl::vector<decltype(*Iterator)>::iterator bufFirst, gtl::vector<decltype(*Iterator)>::iterator bufLast)
+template <typename Iterator, typename BufType, typename Comparator>
+void mergetSort(Iterator first, Iterator last, BufType::iterator bufFirst, Comparator lessThan)
 {
 	if (first < last)
 	{
-		Iterator center = first + gtl::distance(first, last) / 2;
-		gtl::vector<decltype(*Iterator)>::iterator bufCenter = bufFirst + gtl::distance(bufFirst, bufLast) / 2;
-		mergetSort(first, center, bufFirst, bufCenter);
-		mergetSort(center + 1, last, bufCenter + 1, bufLast);
-		merge(first, last, bufFirst, bufLast);
+		int count = gtl::distance(first, last);
+		Iterator center = first + count / 2;
+		BufType::iterator bufCenter = bufFirst + count / 2;
+		mergetSort(first, center, bufFirst, lessThan);
+		mergetSort(center + 1, last, bufCenter + 1, lessThan);
+		merge(first, last, bufFirst, lessThan);
 	}
 }
 
-template <typename Iterator>
-void mergeSort(Iterator first, Iterator last, const std::function<bool (const decltype(*Iterator) &left, const decltype(*Iterator) &right)> &lessThan)
+template <typename Iterator, typename Comparator>
+void mergeSort(Iterator first, Iterator last, Comparator lessThan)
 {
 	vector<decltype(*Iterator)> tmp(distance(first, last));
-	mergeSort(first, last, tmp.begin(), tmp.end());
+	mergeSort(first, last, tmp.begin(), lessThan);
+}
+
+template <typename Iterator>
+void mergeSort(Iterator first, Iterator last)
+{
+	mergeSort(first, last, [](const decltype(*Iterator) &left, const decltype(*Iterator) &right) { return *left < *last; });
 }
 
 #endif // !_SORT_HPP_
