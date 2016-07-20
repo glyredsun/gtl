a9de24d05b47c53da2510c7b0c23fb133cba8d84
gly <glyredsun@163.com>
2016-07-20 16:22:12 +0800

    add lastPOT and nextPOT

--- gtl/algorithm.hpp
+++ gtl/algorithm.hpp
@@ -13,6 +13,22 @@
 NS_BEGIN(gtl);
 
 template <typename T>
+T nextPOT(T num)
+{
+	for (size_t i = 1; i <= sizeof(T) * 8 / 2; i *= 2)
+	{
+		num = num | num >> i;
+	}
+	return num + 1;
+}
+
+template <typename T>
+T lastPOT(T num)
+{
+	return nextPOT(num) >> 1;
+}
+
+template <typename T>
 void swap(T& left, T& right)
 {
 	T tmp = std::move(left);
@@ -37,6 +53,15 @@ int distance(const Iterator first, const Iterator last)
 {
 	return last - first;
 }
+
+template <typename ArgType = void>
+struct less
+{
+	constexpr bool operator() (const ArgType &left, const ArgType &right) const 
+	{
+		return left < right;
+	}
+};
 
 NS_END(gtl);
 
