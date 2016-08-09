e59a96e6db0e9e4a184453c8425e509cf40ea806
glyredsun <glyredsun@163.com>
2016-08-09 21:46:56 +0800

    use _start _finishi _end_of_storage

--- gtl/vector.hpp
+++ gtl/vector.hpp
@@ -44,7 +44,7 @@ public:
 	vector(std::initializer_list<value_type> args)
 		: vector(args.size())
 	{
-		gtl::move(args.begin(), args.end(), _datas, _datas + _size);
+		gtl::move(args.begin(), args.end(), _start);
 	}
 
 	template <typename Iterator>
@@ -66,7 +66,7 @@ public:
 
 	vector(vector&& other)
 	{
-		moveFrom(other);
+		moveFrom(gtl::move(other));
 	}
 
 	vector& operator=(const vector& other)
@@ -93,33 +93,33 @@ public:
 		return _start[idx];
 	}
 
-	bool empty() {
+	bool empty() const {
 		return _start == _finish;
 	}
 
-	size_type size() {
+	size_type size() const {
 		return _finish - _start;
 	}
 
-	size_type capacity() {
+	size_type capacity() const {
 		return _end_of_storage - _start;
 	}
 
 	void resize(size_type newSize) {
-		if (newSize > _capacity) {
+		if (newSize > capacity()) {
 			reserve(newSize * 2);
 		}
 		_finish = _start + newSize;
 	}
 
 	void reserve(size_type newCapacity) {
-		if (newCapacity <= _finish - _start) {
+		if (newCapacity <= size()) {
 			return;
 		}
 
 		value_type *newStart = new value_type[newCapacity];
 
-		std::move(_start, _finish, newStart);
+		gtl::move(_start, _finish, newStart);
 
 		if (_start)
 			delete[] _start;
@@ -128,13 +128,13 @@ public:
 	}
 
 	void push_back(rvalue_reference data) {
-		resize(_size + 1);
-		_finish++ = std::move(data);
+		resize(size() + 1);
+		*_finish++ = std::move(data);
 	}
 
 	void push_back(const_reference data) {
-		resize(_size + 1);
-		_finish++ = data;
+		resize(size() + 1);
+		*_finish++ = data;
 	}
 
 	void pop_back() {
@@ -193,10 +193,11 @@ protected:
 				gtl::move(src, pos, _start);
 			}
 
-			for (size_t i = _size - 1; i >= first; --i)
-			{
-				_start[i + shift] = gtl::move(src[i]);
-			}
+			iterator p = _finish - 1;
+			iterator q = _start + (pos - src) + shift - 1;
+
+			while (p >= pos) 
+				*q-- = *p--;
 
 			if (src != _start) {
 				delete[] src;
