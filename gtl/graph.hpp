ebd9128e34069a3b0c187f45b11452b227df8950
glyredsun <glyredsun@163.com>
2016-08-26 07:59:34 +0800

    add graph

--- gtl/graph.hpp
+++ gtl/graph.hpp
@@ -13,25 +13,43 @@
 
 NS_BEGIN(gtl);
 
-template <class NodeType, class EdgeType, class EdgeContainerType = gtl::list<EdgeType>, class CantainerType = gtl::vector<gtl::pair<NodeType, EdgeContainerType>>>
+template <class NodeType, class EdgeType, class EdgeContainerType = list<EdgeType>, class CantainerType = vector<pair<NodeType, EdgeContainerType>>>
 class sparse_graph
 {
+public:
 	typedef NodeType node_type;
 	typedef EdgeType edge_type;
 	typedef CantainerType container_type;
 	typedef EdgeContainerType edge_container_type;
-	typedef container_type::size_type index_type;
+	typedef typename container_type::size_type index_type;
+	typedef pair<NodeType, EdgeContainerType> value_type;
 
-	const container_type& container() const { return _container; }
-	const edge_container_type& edges(index_type idx) const { return _container[idx].second; }
+	const container_type& container() const { return container_; }
+	const edge_container_type& edges(index_type idx) const { return container_[idx].second; }
 
-	void add_node(const node_type& node) {
-
-	}
+	void add_node(const node_type& node) { container_.push_back(value_type(node, edge_container_type())); }
+	void add_edge(const edge_type& edge) { container_[edge.from()].second.push_back(edge); }
 
 protected:
-	container_type _container;
+	container_type container_;
 };
+
+template <class NodeType, class EdgeType, class EdgeContainerType = list<EdgeType>, class CantainerType = vector<pair<NodeType, EdgeContainerType>>>
+std::ostream& operator<< (std::ostream& o, const sparse_graph<NodeType, EdgeType, EdgeContainerType, CantainerType>& g)
+{
+	auto c = g.container();
+	for (size_t i = 0, len = c.size(); i < len; ++i)
+	{
+		auto value = c[i];
+		o << i << ", [";
+		for (auto edge : value.second)
+		{
+			o << edge.to() << " ";
+		}
+		o << "]" << std::endl;
+	}
+	return o;
+}
 
 NS_END(gtl);
 
--- gtl_tests/graph_test.cpp
+++ gtl_tests/graph_test.cpp
@@ -1,1 +1,34 @@
 #include <graph.hpp>
+
+struct GraphNode
+{
+
+};
+
+struct GraphEdge
+{
+	GraphEdge() {}
+
+	GraphEdge(int from, int to) : from_(from), to_(to)
+	{
+
+	}
+
+	int from() const { return from_; }
+	int to() const { return to_; }
+
+	int from_, to_;
+};
+
+int main(void)
+{
+	gtl::sparse_graph<GraphNode, GraphEdge> g;
+	
+	g.add_node(GraphNode());
+	g.add_node(GraphNode());
+	g.add_edge(GraphEdge(0, 1));
+
+	std::cout << g;
+
+	return 0;
+}
